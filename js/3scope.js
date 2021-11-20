2497b7d64aeb4816763ad6b2c2adbb898339a064
Gael Ramos <esgaelramos@gmail.com>
2021-11-19 19:40:22 -0600

    Create 3scope.js

---
+++ js/3scope.js
@@ -0,0 +1,8 @@
+// ------------- SCOPE ------------- // 
+var miNombre = "Diego";
+function nombre(){
+    var miApellido = "De Granda";
+    console.log(miNombre + " " + miApellido);    
+}
+nombre();
+//--D Diego De Granda
