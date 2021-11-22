56709b45011c42bb093224f1bed53ce4243ef097
Gael Ramos <esgaelramos@gmail.com>
2021-11-21 19:47:56 -0600

    Create switch.js

---
+++ js/7if.js
@@ -0,0 +1,24 @@
+// ------------- IF ------------- // 
+if (false) {
+    console.log("Hola");
+} else if (true) {
+    console.log("Soy Medio Falso");
+} else {
+    console.log("Soy Falso");
+}
+//para saber si puedes votar
+var edad = 18;
+
+if(edad === 18) {
+    console.log("Puedes votar por primera vez");
+} else if (edad > 18) {
+    console.log("Puedes votar de nuevo");
+} else {
+    console.log("Todavía no puedes votar");
+}
+
+condition ? true : false;
+
+var num = 1;
+
+var resultado = num === 1 ? "Sí soy un 1" : "No iguales";
---
+++ js/9switch.js
@@ -0,0 +1,15 @@
+// ------------- SWITCH ------------- // 
+var numero = 1;
+switch(numero){
+    case 1:
+        console.log("Soy uno!");
+        break;
+    case 10:
+        console.log("Soy un diez!");
+        break;
+    case 88:
+        console.log("Soy un ochenta y ocho!");
+        break;
+    default:
+        console.log("No soy nada, bueno sí un: " + numero);
+}
