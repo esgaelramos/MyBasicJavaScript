3edbf57ab5f4d007e38a0c81357fd3c20875433a
Gael Ramos <esgaelramos@gmail.com>
2021-11-23 19:50:51 -0600

    Create 11arrays.js

---
+++ js/11arrays.js
@@ -0,0 +1,17 @@
+// -------------  ARRAYS ------------- //
+var frutas = ["manzana", "platano", "cereza", "fresa"];
+
+console.log(frutas);
+//--D (4) ['manzana', 'platano', 'cereza', 'fresa']
+
+
+console.log(frutas.length);
+//para saber cuantos elementos tiene este array
+//--D 4
+
+console.log(frutas[0]);
+//para acceder a elementos específicos
+//--D manzana
+
+//recuerda que existe el index, y al contar se va desde 0 a... X
+
