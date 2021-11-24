3007494e254481c13428b2b2584d10ff49a922bc
Gael Ramos <esgaelramos@gmail.com>
2021-11-23 19:51:47 -0600

    I create 12take-arrays.js

---
+++ js/12take-arrays.js
@@ -0,0 +1,16 @@
+//MÉTODOS PARA MUTAR ARRAYS
+
+var masFrutas = frutas.push("uvas");
+//añade elementos al final del array / .push()
+
+var ultimo = frutas.pop("uvas");
+//elimina el ultimo elemento del array / .pop()
+
+var nuevaLongitud = frutas.unshift("uvas");
+//agrega elementos al principio del array / .unshift()
+
+var nuevaLongitud = frutas.shift("uvas");
+//borra el último elemento al princio del array / .shift()
+
+var posicion = frutas.indexOf("cereza")
+//muestra el index del elemento / indexOf
